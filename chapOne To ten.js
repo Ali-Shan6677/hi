ed701bef9b23221ba68f749d8f7302fd9336f53e
Ali-Shan6677 <alimkd6677@gmail.com>
2024-09-05 08:49:22 +0500

    Add files via upload

---
+++ chapOne To ten.js
@@ -0,0 +1,152 @@
+//alert(hello world)
+//alert(2+2)
+//console.log("Pakistan")
+    
+   // document.write("Pakistan 322")
+
+  // console.warn("Pakistan")
+
+
+  //for print the data 
+
+  //....................................................
+
+  //   variables for string
+
+  //let firstname = "Ali"
+  //let secName = "khan"
+  //console.log("student Name "+firstname+secName)
+ // console.log(`student Name $StudentName}`)
+ //......................................................
+//    Variables for numbers
+
+//let age=23;
+//console.log("age: " + age);
+
+//let rs= 100;
+
+//const price = 25;
+//price=20;
+//console.log(price)
+//console.log("mango:" +rs);
+
+//...........................................................
+
+//console.log(9+2)
+//console.log(10-5)
+//console.log(5*3)
+//console.log(25/5)
+//console.log(20%2)
+//console.log(2+ 3 -8 * 2 / 4 +3 - 9)
+
+//let num1 = prompt("enter number one")
+//let num2 = prompt ("enter number two")
+//console.log(num1-num2)
+
+
+//let num1 = prompt("please inter number one") 
+   // let num2 =  prompt("please inter number Two") 
+    //console.log( num1 - num2)
+
+   // console.log(2* 2* 5 -5 / 2 + 15 )  
+
+   //let rs= ("1+1")
+
+
+
+
+// let email = "Alishan@gmail.com";
+// let pass = "ali";
+
+// email = prompt("Enter your email")
+// password = prompt("Enter the password")
+
+// if(email == "Alishan@gmail.com"){
+
+//    if(pass = "ali"){
+//       console.log("Your are logged in")
+//    }
+//    else{
+//       console.log("Email  is incorrect")
+// }
+// }
+// else{
+//    console.log("passward is wrong")
+// }
+
+
+// let cities = ["Karachi" ,"Peshawar" , "Islamabad" , "lahore" ,"Swat "]
+
+// console.log(cities[4])
+
+
+
+// let marks = []
+// console.logH(marks);
+// marks.push(41, 53,54,67,43,)
+// console.log(marks [4])
+
+
+
+// let age = 3
+// if (age > 19){
+
+// console.log("you can")
+
+// }
+  
+// console.log ( age < 22 )
+
+
+// let age = prompt( "please enter your age" )
+
+// if ( age > 18) 
+//    {
+//    console.log("you can make CNIC")
+// }
+
+// else{
+//    console.log ("sorry you are too younger")
+// }
+
+
+
+// let km = prompt("enter your km")
+// if (km >10 ){
+//    console.log ("pay rs 40")
+// }
+// else{
+//    console.log ("go somewhere")
+// }
+
+
+
+
+// let pass = prompt("plz enter your passward")
+
+// if(pass == 12345) {
+
+//    console.log("open the door")
+// }
+
+// else {
+//    console.log ("who are you ?")
+// }
+
+
+
+
+
+
+
+// let km = prompt("enter your km")
+
+// if (km > 10){
+
+//    console.log ("pay rs 100")}
+
+//    else {
+//  console.log("go somewhere")
+//    }
+
+
